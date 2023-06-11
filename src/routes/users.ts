4255c57e5b599cae09c56d295fb8301938c135d8
Enz0w <endooliveira49@gmail.com>
2023-06-11 16:23:50 -0300

    fix: removed test files and unused middleware verifications

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,7 +2,6 @@ import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
-import { checkSessionIdExists } from '../middlewares/check-user-id-exists'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.get('/', async () => {
@@ -10,27 +9,21 @@ export async function usersRoutes(app: FastifyInstance) {
     return { users }
   })
 
-  app.get(
-    '/:id',
-    {
-      preHandler: [checkSessionIdExists],
-    },
-    async (request) => {
-      const getUserParamsSchema = z.object({
-        id: z.string().uuid(),
+  app.get('/:id', async (request) => {
+    const getUserParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getUserParamsSchema.parse(request.params)
+
+    const user = await knex('users')
+      .where({
+        id,
       })
+      .first()
 
-      const { id } = getUserParamsSchema.parse(request.params)
-
-      const user = await knex('users')
-        .where({
-          id,
-        })
-        .first()
-
-      return { user }
-    },
-  )
+    return { user }
+  })
 
   app.post('/', async (request, response) => {
     const createUserBodySchema = z.object({
